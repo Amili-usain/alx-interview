955fc386a5180e5c8230a8b84ffcf617f3414b43
Amili-usain <amilihussein073@gmail.com>
2023-05-09 22:23:02 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,53 +1,43 @@
 #!/usr/bin/python3
 """This script reads stdin line by line and computes metrics:"""
 
-import re
 import sys
 
+# Initialize a dictionary to cache status codes and their frequency
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 
-def output(log: dict) -> None:
-    """
-    helper function to display stats
-    """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+# Initialize the total size of the file
+total_size = 0
 
+# Initialize a counter to print stats every 10 lines
+counter = 0
 
-if __name__ == "__main__":
-    regex = re.compile(
-          r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - '
-          r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] '
-          r'"GET /projects/260 HTTP/1.1" (.{3}) (\d+)'
-    )
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500
-        ]
-    }
+        # Print stats every 10 lines
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if match:
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
+except Exception as err:
+    pass
 
-                # File size
-                log["file_size"] += file_size
-
-                # status code
-                if code.isdecimal():
-                    log["code_frequency"][code] += 1
-
-                if line_count % 10 == 0:
-                    output(log)
-    finally:
-        output(log)
+finally:
+    # Print the final stats
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
